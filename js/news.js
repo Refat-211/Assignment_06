a4297b9cfc1796b7d3f94bf0561481a84acaf273
unknown <refathassan512@gmail.com>
2022-09-04 08:53:59 +0600

    show news card

--- js/news.js
+++ js/news.js
@@ -1,18 +1,126 @@
-const loadCategories = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-        .then(res => res.json())
-        .then(data => displayCategories(data.data.news_category));
+// Load Categories
+
+const loadCategories = async () => {
+    try {
+        const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
+        const data = await res.json()
+        displayCategories(data.data.news_category)
+    }
+    catch (error) {
+        console.log(error);
+    }
+        
 }
 
-const displayCategories = categories => {
+const displayCategories = async (categories) => {
     const categoryContainer = document.getElementById('category-container');
     categories.forEach(category => {
+        const {category_name, category_id} = category;
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
-            <button onclick="newsCategory(${category.category_id})" class="border rounded my-1 border-primary btn btn-outline-primary" href="">${category.category_name}</button>
+            <button onclick="loadCards('${category_id}')" class="border rounded my-1 border-primary btn btn-outline-primary">${category_name}</button>
         `;
         categoryContainer.appendChild(categoryDiv);
-    })
+    });
 }
+
+// News Card
+
+const loadCards = async (categoryId) => {
+
+    try {
+        const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
+        const data = await res.json()
+        displayCards(data.data)
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+const displayCards = (newsCards) => {
+
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = '';
+
+    const newsItems = document.getElementById('news-items-number')
+    newsItems.classList.remove('d-none');
+
+    const showItems = document.getElementById('show-items-numbers')
+    showItems.innerText = newsCards.length;
+
+    if(newsCards.length === 0){
+        const spinnerContainer = document.getElementById('spinner')
+        spinnerContainer.classList.add('d-none');
+    }
+
+
+// news card sort
+
+    newsCards.sort((a, b) => {
+        return b.total_view - a.total_view;
+    });
+
+    newsCards.forEach(card => {
+
+        const {thumbnail_url, title, details, author, total_view} = card;
+        const { name, published_date, img } = author; 
+
+        const newsCardDiv = document.createElement('div');
+
+// spinner
+        const spinner = document.getElementById('spinner')
+        spinner.classList.add('d-none');
+
+        newsCardDiv.innerHTML = `
+        
+            <div class="card mb-3" style="max-width: full;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${thumbnail_url}" class="rounded-start" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title fs-4 fw-bold">${title}</h5>
+                            <p class="card-text">${details.length > 400 ? details.slice(0, 400) + "..." : details}</p>
+                        </div>
+                        <div class="d-lg-flex flex-lg-row flex-sm-column ms-3 mt-2 justify-content-lg-between">
+                            <div class="d-flex">
+                                <div>
+                                    <img class="rounded-circle" width="40px" height="40px" src="${img ? img : "img not found"}" alt="">
+                                </div>
+                                <div class="ms-2">
+                                    <h6 class="mb-0">${name ? name : "name not found"}</h6>
+                                    <p>${published_date ? published_date : "published date not found"}</p>
+                                </div>
+                            </div>
+                            <div class="d-flex py-2">
+                                <p class="fs-5"><i class="fa-solid fs-5 fa-eye"></i> ${total_view || total_view === 0 ? total_view : "view not found"}</p>
+                            </div>
+                            <div>
+                                <button class="my-2 me-2 p-1 text-light border rounded border-primary bg-primary bg-gradient"  onclick="modal('${card._id}')">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        `
+        cardContainer.appendChild(newsCardDiv);
+
+    });
+
+}
+
+
+
+
+
+
+
+
+loadCards('01');
+
 
 loadCategories();
